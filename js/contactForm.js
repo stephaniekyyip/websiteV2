28a68f9d330b15443c38edb384fcf0dfd97e3fd1
stephaniekyyip <stephaniekyyip@gmail.com>
2018-03-24 00:18:55 -1000

    Minor changes to footer on Contact page. Added form validation in js.

--- js/contactForm.js
+++ js/contactForm.js
@@ -1,22 +1,22 @@
 
 function validateEmail(){
 	var checkEmail = document.contactForm.email.value;
-				
+
 	atpos = checkEmail.indexOf("@");
     dotpos = checkEmail.lastIndexOf(".");
-         
+
     if (atpos < 1 || ( dotpos - atpos < 2 )) {
     	return false;
     }
-    
+
     return true;
 }
-			
-function validateForm(){	
-	var checkName = document.forms["contactForm"]["senderName"].value;
+
+function validateForm(){
+	var checkName = document.forms["contactForm"]["name"].value;
 	var checkMsg = document.forms["contactForm"]["msg"].value;
 	var checkEmail = document.forms["contactForm"]["email"].value;
-		
+
 	if (checkName == ""){
 		alert("Please type your name! I want to know who is sending the message.");
 		return false;
@@ -30,31 +30,32 @@ function validateForm(){
 		alert("Please type a message!");
 		return false;
 	}
-		
-	return true;	
+
+	return true;
 
 }
 
+/*
 function submitForm(){
 	var sender = document.forms["contactForm"]["senderName"].value;
 	var msg = document.forms["contactForm"]["msg"].value;
 	var email = document.forms["contactForm"]["email"].value;
-		
+
 	if(validateForm() == true){
 		var xhttp = new XMLHttpRequest();
 		var response = "";
-		
+
 		/*xhttp.onreadystatechange = function() {
 			if(this.readyState == 4 && this.status = 200){
 				response  = this.responseText;
 			}
-		
-		};*/
-		
-		xhttp.open("POST", "/sendEmail.php?sender=" + sender + "&email=" + email 
+
+		};
+
+		xhttp.open("POST", "/sendEmail.php?sender=" + sender + "&email=" + email
 		+ "&msg=" + msg, true);
 		xhttp.send();
-		
+
 		if (response == 1){
 			alert("E-mail sent!");
 			return true;
@@ -62,10 +63,9 @@ function submitForm(){
 			alert("Error! Unable to send e-mail!");
 			return false;
 		}
-	
+
 	}
 	return false;
 
-}			
-
-		
+}
+*/
