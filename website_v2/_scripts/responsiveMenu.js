d8b51dac9092b909548f6f9864f56b70df05d712
Stephanie Yip <stephaniekyyip@gmail.com>
2018-06-22 11:25:23 -1000

    Minor changes (refactoring, design, text).

--- website_v2/_scripts/responsiveMenu.js
+++ website_v2/_scripts/responsiveMenu.js
@@ -1,25 +1,19 @@
-// Menu in responsive/ mobile version
+// ----------------------------------------------------------------------------
+// Dropdown menu in responsive version
+// ----------------------------------------------------------------------------
 
-var icon = document.getElementById("menu-icon");
-var menu = document.getElementById("icon-nav-links");
+var icon = document.getElementById('menu-icon');
+var menu = document.getElementById('icon-nav-links');
 
 icon.addEventListener("click", function(){
   // Toggle dropdown menu
   $('#icon-nav-links').slideToggle();
 
   // Toggle menu icon from hamburger to X
-  if(icon.classList.contains('fa-bars')){
-    icon.classList.add('fa-times');
-    icon.classList.remove('fa-bars');
-  }else{
-    icon.classList.add('fa-bars');
-    icon.classList.remove('fa-times');
-  }
+  icon.classList.toggle('fa-times');
+  icon.classList.toggle('fa-bars');
 
   // Toggle color of menu icon
-  if(icon.classList.contains('icon-nav-active')){
-    icon.classList.remove('icon-nav-active');
-  }else{
-    icon.classList.add('icon-nav-active');
-  }
+  icon.classList.toggle('icon-nav-active');
+
 });
